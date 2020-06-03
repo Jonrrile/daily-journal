0bc8861b61061708c760feb6093fc03ace4dd4f1
Jonrrile <jonrrile@ut.utm.edu>
2020-06-02 20:06:15 -0500

    adding initial JS files

---
+++ scripts/journal.js
@@ -0,0 +1,15 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const journalEntry = {
+    date: "June 2nd 2020",
+    concept: "JavaScript Objects",
+    entry: "Today we learned about automating HTML entries utilizing JavaScript",
+    mood: "Confused"
+
+
+}
+
+var allJournalEntries = [];
+allJournalEntries.push(journalEntry);
