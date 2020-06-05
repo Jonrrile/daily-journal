1b4dae0d91280ed1e9b95cd00d1aa0095079b9d1
jonrrile <jonrrile@ut.utm.edu>
2020-06-05 13:12:15 -0500

    changes didn't save

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -33,22 +33,3 @@ const renderJournalEntries = () => {
 }
 
 // Invoke the render function
-
-let journalEntry = []
-
-const getJournalEntry = () => {
-    return fetch("http://localhost:3000/journalEntry") // Fetch from the API
-    .then((httpResponse) => {
-        return httpResponse.json()
-    }  
-    )// Parse as JSON
-    
-    .then(
-        (entries) => {
-        // What should happen when we finally have the array?
-            journalEntry = entries 
-        }
-    )
-}
-
-renderJournalEntries();
